279dbfa8451c4db35eccff5d839395ad4e83cc14
bspowell <82974206+bspowell@users.noreply.github.com>
2023-08-29 10:09:43 -0700

    Aug 29th - links, css, edit form

--- cms.rb
+++ cms.rb
@@ -3,30 +3,68 @@ require "sinatra/reloader"
 require "tilt/erubis"
 require "redcarpet"
 
-root = File.expand_path("..", __FILE__)
-
 configure do
   enable :sessions
   set :session_secret, "46032b19138ea43d9ad6e96bbd85bca9d29d8c0084e0c093300a85dae7b96195"
 end 
 
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
+
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+def load_file_content(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    erb render_markdown(content)
+  end
+end
+
 get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|  # dir.glob gives back an array
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|  # dir.glob gives back an array
     File.basename(path)
   end
-
   erb :index
 end
 
-# cms.rb
 get "/:filename" do
-  file_path = root + "/data/" + params[:filename]
+  file_path = File.join(data_path, params[:filename])
 
-  if File.file?(file_path)
-    headers["Content-Type"] = "text/plain"
-    File.read(file_path)
+  if File.exist?(file_path)
+    load_file_content(file_path)
   else
     session[:message] = "#{params[:filename]} does not exist."
     redirect "/"
   end
+end
+
+get "/:filename/edit" do
+  file_path = File.join(data_path, params[:filename])
+
+  @file_name = params[:filename]
+  @content = File.read(file_path)
+
+  erb :edit
+end
+
+post "/:filename" do
+  file_path = File.join(data_path, params[:filename])
+
+  File.write(file_path, params[:content])
+
+  session[:message] = "#{params[:filename]} is now updated"
+  redirect "/"
 end
--- test/cms_test.rb
+++ test/cms_test.rb
@@ -1,9 +1,12 @@
+# test/cms_test.rb
 ENV["RACK_ENV"] = "test"
+
+require "fileutils"
 
 require "minitest/autorun"
 require "rack/test"
 
-require_relative "../cms.rb"
+require_relative "../cms"
 
 class CMSTest < Minitest::Test
   include Rack::Test::Methods
@@ -12,34 +15,84 @@ class CMSTest < Minitest::Test
     Sinatra::Application
   end
 
-  def test_index
-    get "/"
-    assert_equal 200, last_response.status
-    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
-    assert_includes last_response.body, "about.txt"
-    assert_includes last_response.body, "changes.txt"
-    assert_includes last_response.body, "history.txt"
+  def setup
+    FileUtils.mkdir_p(data_path)
   end
 
-  def test_viewing_text_doc
+  def teardown
+    FileUtils.rm_rf(data_path)
+  end
+
+  def create_document(name, content = "")
+    File.open(File.join(data_path, name), "w") do |file|
+      file.write(content)
+    end
+  end
+
+  def test_index
+    create_document "about.md"
+    create_document "changes.txt"
+
+    get "/"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.md"
+    assert_includes last_response.body, "changes.txt"
+  end
+
+  def test_viewing_text_document
+    create_document "history.txt", "Ruby 0.95 released"
+
     get "/history.txt"
+
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
-    assert_equal File.read("data/history.txt"), last_response.body
-    # assert_includes last_response.body, "Ruby 0.95 released"
+    assert_includes last_response.body, "Ruby 0.95 released"
+  end
+
+  def test_viewing_markdown_document
+    create_document "about.md", "# Ruby is..."
+
+    get "/about.md"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>Ruby is...</h1>"
   end
 
   def test_document_not_found
-    get "/notafile.ext" # Attempt to access a nonexistent file
-  
-    assert_equal 302, last_response.status # Assert that the user was redirected
-  
-    get last_response["Location"] # Request the page that the user was redirected to
-  
+    get "/notafile.ext"
+
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+
     assert_equal 200, last_response.status
     assert_includes last_response.body, "notafile.ext does not exist"
-  
-    get "/" # Reload the page
-    refute_includes last_response.body, "notafile.ext does not exist" # Assert that our message has been removed
+  end
+
+  def test_editing_document
+    create_document "changes.txt"
+
+    get "/changes.txt/edit"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<textarea"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  def test_updating_document
+    post "/changes.txt", content: "new content"
+
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+
+    assert_includes last_response.body, "changes.txt has been updated"
+
+    get "/changes.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "new content"
   end
 end
